016186fed997bdf5575b0c3c2bf05738b3a8de33
jgiron730 <badum7@gmail.com>
2020-06-15 11:55:15 -0600

    Updates

---
+++ precache-manifest.acda7c53fab4650bc4918ff3c1bd9f0d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f76f092e9a2ecc208fa2e5d6f805e8f",
+    "url": "/store-redux-react/index.html"
+  },
+  {
+    "revision": "6d21a827cec9d1171108",
+    "url": "/store-redux-react/static/css/main.1dde8147.chunk.css"
+  },
+  {
+    "revision": "47d9801ea32b9ea460cd",
+    "url": "/store-redux-react/static/js/2.caf68b66.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/store-redux-react/static/js/2.caf68b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d21a827cec9d1171108",
+    "url": "/store-redux-react/static/js/main.c0d6900d.chunk.js"
+  },
+  {
+    "revision": "40fb632a822a760d65b7",
+    "url": "/store-redux-react/static/js/runtime-main.642a947d.js"
+  },
+  {
+    "revision": "d9d48f4cd0f71bccd43d9c25a26f837b",
+    "url": "/store-redux-react/static/media/bookcase.d9d48f4c.jpg"
+  },
+  {
+    "revision": "8c12970442838ed1bbfa17b3d7972497",
+    "url": "/store-redux-react/static/media/cama.8c129704.jpg"
+  },
+  {
+    "revision": "fef455b848353d43dc972dd30df5b49f",
+    "url": "/store-redux-react/static/media/chair.fef455b8.jpg"
+  },
+  {
+    "revision": "c7c502478c4e7036eb369d4a4cf2f884",
+    "url": "/store-redux-react/static/media/oven.c7c50247.jpg"
+  },
+  {
+    "revision": "dc41f5eb83d8931cfb3c7d9ee64f1bd1",
+    "url": "/store-redux-react/static/media/pc.dc41f5eb.jpg"
+  },
+  {
+    "revision": "a7acaca19337e7bf3cf7f7a5847119c4",
+    "url": "/store-redux-react/static/media/refrigerator.a7acaca1.jpg"
+  },
+  {
+    "revision": "8a9821748b949fdc2574232d41006a92",
+    "url": "/store-redux-react/static/media/table.8a982174.jpg"
+  },
+  {
+    "revision": "ca86a826eea554cde39e80c85ddb223c",
+    "url": "/store-redux-react/static/media/tv.ca86a826.jpg"
+  }
+]);
